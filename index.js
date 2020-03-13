27e02cf6f83035f80e8467b6eb626826cf0447b5
schauhan <schauhan@cainc.com>
2020-03-13 14:20:05 +0530

    added description

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function getNewToken(oAuth2Client, callback) {
 }
 
 /**
- * Prints the names and majors of students in a sample spreadsheet:
+ * Fetch data of range from spreadsheet:
  * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
